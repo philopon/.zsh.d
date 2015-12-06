c82aa1d2910ab600bef5bd513a644f18d278de9b
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2015-12-07 00:11:23 +0900

    add version check to zsh-users/zsh-syntax-highlighting

--- plugins.zsh
+++ plugins.zsh
@@ -1,8 +1,9 @@
+zplug b4b4r07/zplug
+
 zplug seebi/dircolors-solarized
-zplug zsh-users/zsh-syntax-highlighting
+zplug zsh-users/zsh-syntax-highlighting, if:"__version_requirement '$ZSH_VERSION' '4.3.17'"
 zplug mollifier/anyframe
 zplug zsh-users/zsh-completions
 zplug jocelynmallon/zshmarks
 zplug junegunn/fzf, as:command, do:./install --bin
 zplug themes/steeef, from:oh-my-zsh
-zplug b4b4r07/zplug
