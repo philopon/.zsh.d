1cae380a167f61c8dc1e682a3654f761acb8967c
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2015-12-07 00:33:28 +0900

    fixed fzf

--- plugins.zsh
+++ plugins.zsh
@@ -5,5 +5,6 @@ zplug zsh-users/zsh-syntax-highlighting, if:"__version_requirement '$ZSH_VERSION
 zplug mollifier/anyframe
 zplug zsh-users/zsh-completions
 zplug jocelynmallon/zshmarks
-zplug junegunn/fzf, as:command, do:./install --bin
+zplug junegunn/fzf, as:command, do:"./install --bin", of:"bin/fzf"
+
 zplug themes/steeef, from:oh-my-zsh
