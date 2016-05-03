38cccbac22c6e29b6466dbbbb4e043464f49e009
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-05-03 22:29:14 +0900

    fix direnv os

--- packages.zsh
+++ packages.zsh
@@ -2,6 +2,10 @@ version_requirement() {
     __zplug::core::core::version_requirement "$@"
 }
 
+get_os() {
+    __zplug::core::core::get_os
+}
+
 zplug zsh-users/zsh-syntax-highlighting, nice:10, if:"version_requirement $ZSH_VERSION 4.3.17"
 zplug zsh-users/zsh-completions
 
@@ -19,6 +23,6 @@ zplug $ZDOTDIR/local/config, from:local
 zplug $ZDOTDIR/local/aliases, from:local, nice:5
 zplug $ZDOTDIR/local/auto-tmux, from:local, if:"which tmux > /dev/null", nice:-5
 
-zplug direnv/direnv, as:command, from:gh-r, hook-build:"chmod 755 *", use:"*", rename-to:direnv, hook-load:'eval "$(direnv hook zsh)"'
+zplug direnv/direnv, as:command, from:gh-r, hook-build:"chmod 755 *", use:"*$(get_os)*", rename-to:direnv, hook-load:'eval "$(direnv hook zsh)"'
 
 zplug $ZDOTDIR/local/pip-wrapper, from:local
