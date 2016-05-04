70b99eec95146572783f0fe748c57baa989034c8
philopon <philopon.dependence@gmail.com>
2016-05-05 01:32:10 +0900

    supress which output

--- local/aliases/ls.zsh
+++ local/aliases/ls.zsh
@@ -1,4 +1,4 @@
-if which gls > /dev/null; then
+if which gls &> /dev/null; then
     alias ls="gls --color"
 elif [[ $OSTYPE == *linux* ]]; then
     alias ls="ls --color"
--- local/aliases/vim.zsh
+++ local/aliases/vim.zsh
@@ -1,6 +1,6 @@
-if which nvim > /dev/null; then
+if which nvim &> /dev/null; then
     alias vim=nvim
-elif which Vim > /dev/null; then
+elif which Vim &> /dev/null; then
     alias vim=Vim
 fi
 
