cd6688ab9ac797ca3d49ae8a1071343c98397d42
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-05-17 02:43:17 +0900

    fix attach-tmux

--- scripts/attach-tmux.zsh
+++ scripts/attach-tmux.zsh
@@ -1,41 +1,43 @@
-if hash tmux &> /dev/null; then
+() {
+    if hash tmux &> /dev/null; then
 
-    attach-tmux () {
-        local result=`tmux list-session 2> /dev/null`
-        if [[ -z "$result" ]]; then
-            tmux new-session
-            return $?
+        attach-tmux () {
+            local result=`tmux list-session 2> /dev/null`
+            if [[ -z "$result" ]]; then
+                tmux new-session
+                return $?
+            fi
+
+            local list=`
+            cat <(echo "$result" | grep -v '(attached)$') <(echo :: NEW SESSION)
+            `
+
+            result=`echo "$list" | fzf --exit-0`
+            if [[ $? != 0 ]]; then
+                tmux new-session
+                return $?
+            fi
+
+            result=`echo $result | cut -d: -f1`
+            echo $result
+
+            if [[ -z "$result" ]]; then
+                tmux new-session
+                return $?
+            fi
+
+            tmux attach-session -t $result
+        }
+
+        [[ -z "$SSH_CONNECTION" ]] && return
+
+        if [[ -z "$TMUX" ]]; then
+            [[ ! -f ~/.tmux.conf ]] && ln -sf $ZDOTDIR/tmux.conf ~/.tmux.conf
+            [[ ! -d ~/.tmux/plugins/tpm ]] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+            exec attach-tmux
+
+        elif ((`grep @plugin $ZDOTDIR/tmux.conf | wc -l` - `ls ~/.tmux/plugins|wc -l` > 0)); then
+            tmux run-shell ~/.tmux/plugins/tpm/bindings/install_plugins
         fi
-
-        local list=`
-        cat <(echo "$result" | grep -v '(attached)$') <(echo :: NEW SESSION)
-        `
-
-        result=`echo "$list" | fzf --exit-0`
-        if [[ $? != 0 ]]; then
-            tmux new-session
-            return $?
-        fi
-
-        result=`echo $result | cut -d: -f1`
-        echo $result
-
-        if [[ -z "$result" ]]; then
-            tmux new-session
-            return $?
-        fi
-
-        tmux attach-session -t $result
-    }
-
-    [[ -z "$SSH_CONNECTION" ]] && break
-
-    if [[ -z "$TMUX" ]]; then
-        [[ ! -f ~/.tmux.conf ]] && ln -sf $ZDOTDIR/tmux.conf ~/.tmux.conf
-        [[ ! -d ~/.tmux/plugins/tpm ]] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-        exec attach-tmux
-
-    elif ((`grep @plugin $ZDOTDIR/tmux.conf | wc -l` - `ls ~/.tmux/plugins|wc -l` > 0)); then
-        tmux run-shell ~/.tmux/plugins/tpm/bindings/install_plugins
     fi
-fi
+}
