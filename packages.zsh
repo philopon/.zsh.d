7ea150222fef80a2c913ce36a4c124d1f83e0855
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-01-04 02:18:50 +0900

    update zplug

--- packages.zsh
+++ packages.zsh
@@ -6,9 +6,7 @@ zsh_version() {
     __zplug::base::base::version_requirement $ZSH_VERSION $1
 }
 
-zplug zplug/zplug
-
-zsh_version 4.3.17 && zplug zsh-users/zsh-syntax-highlighting, nice:10
+zsh_version 4.3.17 && zplug zsh-users/zsh-syntax-highlighting, defer:2
 zplug zsh-users/zsh-completions
 
 zplug $ZDOTDIR/local/prompt, from:local
@@ -16,18 +14,18 @@ zplug $ZDOTDIR/local/prompt, from:local
 zplug $ZDOTDIR/local/register-lscolors, from:local
 
 local DIRCOLORS_FILE=seebi/dircolors-solarized/dircolors.256dark
-zplug ${DIRCOLORS_FILE:h}, hook-load:"register-lscolors $ZPLUG_HOME/repos/$DIRCOLORS_FILE $ZDOTDIR/dircolors", nice:5
+zplug ${DIRCOLORS_FILE:h}, hook-load:"register-lscolors $ZPLUG_HOME/repos/$DIRCOLORS_FILE $ZDOTDIR/dircolors", on:register-lscolors
 
 zplug junegunn/fzf-bin, as:command, from:gh-r, rename-to:fzf
 zplug mollifier/anyframe, hook-load:"source $ZDOTDIR/scripts/hook-anyframe.zsh"
 
-zplug $ZDOTDIR/local/listup-pathenv, from:local, nice:-10
+zplug $ZDOTDIR/local/listup-pathenv, from:local
 
 zplug $ZDOTDIR/local/config, from:local
-zplug $ZDOTDIR/local/aliases, from:local, nice:5
+zplug $ZDOTDIR/local/aliases, from:local
 
 if [[ `hostname` =~ "^fe01p[0-9]{2}$" ]]; then
     zplug philopon/k.zsh, hook-build:make
 fi
 
-zplug direnv/direnv, as:command, from:gh-r, hook-build:"chmod 755 *", use:"*$(get_os)*", rename-to:direnv, hook-load:'eval "$(direnv hook zsh)"'
+zplug motemen/ghq, as:command, from:gh-r, rename-to:ghq
--- zshenv
+++ zshenv
@@ -4,11 +4,14 @@ ZDOTDIR=~/.zsh.d
 ZCACHEDIR=~/.cache/zsh
 ZPLUG_HOME=$ZCACHEDIR/zplug
 
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 export EDITOR=`which vim`
 export PAGER=`which less`
 
-export GOPATH=$HOME/.go
+export GOPATH=$HOME/.go:$HOME/.ghq
 
-PATH=$GOPATH/bin:~/.local/bin:/usr/local/bin:$PATH
+PATH=$HOME/.go/bin:~/.local/bin:/usr/local/bin:$PATH
 
 [[ ! -d $ZCACHEDIR ]] && mkdir -p $ZCACHEDIR
